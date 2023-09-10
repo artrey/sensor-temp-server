024db730c297db43f5157384e1f6e907d6adf082
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2023-09-10 20:51:40 +0300

    chore: style

--- src/main.cpp
+++ src/main.cpp
@@ -179,10 +179,12 @@ void configureOTA()
   ArduinoOTA.onStart([]() {
 #ifdef NEED_SERIAL_PRINT
     String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
+    if (ArduinoOTA.getCommand() == U_FLASH)
+    {
       type = "sketch";
     }
-    else {  // U_FS
+    else
+    {
       type = "filesystem";
     }
     Serial.println("OTA | Start updating " + type);
@@ -204,19 +206,24 @@ void configureOTA()
   ArduinoOTA.onError([](ota_error_t error) {
 #ifdef NEED_SERIAL_PRINT
     Serial.printf("OTA | Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
+    if (error == OTA_AUTH_ERROR)
+    {
       Serial.println("Auth Failed");
     }
-    else if (error == OTA_BEGIN_ERROR) {
+    else if (error == OTA_BEGIN_ERROR)
+    {
       Serial.println("Begin Failed");
     }
-    else if (error == OTA_CONNECT_ERROR) {
+    else if (error == OTA_CONNECT_ERROR)
+    {
       Serial.println("Connect Failed");
     }
-    else if (error == OTA_RECEIVE_ERROR) {
+    else if (error == OTA_RECEIVE_ERROR)
+    {
       Serial.println("Receive Failed");
     }
-    else if (error == OTA_END_ERROR) {
+    else if (error == OTA_END_ERROR)
+    {
       Serial.println("End Failed");
     }
 #endif
