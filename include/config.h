602ffb9bef0872b3dca3733fd41163468ed7a443
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2023-09-10 21:46:24 +0300

    fix: include guard

--- include/config.h
+++ include/config.h
@@ -1,41 +1,46 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
 #include <Arduino.h>
 #include <EEPROM.h>
 
 struct ConfigData {
-    char ssid[24];
-    char pass[24];
-    uint8_t ip[4];
-    uint16_t port;
-    char path[64];
+  char ssid[24];
+  char pass[24];
+  uint8_t ip[4];
+  uint16_t port;
+  char path[64];
 };
 
 class EEPROMConfig
 {
 private:
-    int m_offset;
+  int m_offset;
 
 public:
-    ConfigData data;
+  ConfigData data;
 
 public:
-    EEPROMConfig(int offset) : m_offset(offset) {}
+  EEPROMConfig(int offset) : m_offset(offset) {}
 
-    ConfigData const& read()
-    {
-        return EEPROM.get<ConfigData>(m_offset, data);
-    }
+  ConfigData const& read()
+  {
+    return EEPROM.get<ConfigData>(m_offset, data);
+  }
 
-    ConfigData const& write()
-    {
-        auto& result = EEPROM.put<ConfigData>(m_offset, data);
-        EEPROM.commit();
-        return result;
-    }
+  ConfigData const& write()
+  {
+    auto& result = EEPROM.put<ConfigData>(m_offset, data);
+    EEPROM.commit();
+    return result;
+  }
 
-    ConfigData const& write(int pos, byte value)
-    {
-        EEPROM.put<byte>(m_offset + pos, value);
-        EEPROM.commit();
-        return read();
-    }
+  ConfigData const& write(int pos, byte value)
+  {
+    EEPROM.put<byte>(m_offset + pos, value);
+    EEPROM.commit();
+    return read();
+  }
 };
+
+#endif
